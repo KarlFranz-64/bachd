57a9e077edf15ed163d86c56d023a82bfc29e5f9
Demetry Turi <81445527+KarlFranz-64@users.noreply.github.com>
2025-02-20 04:08:37 +0200

    add .iso images support

--- bachd.sh
+++ bachd.sh
@@ -10,10 +10,26 @@ if ! command -v chdman &> /dev/null; then
     exit 1
 fi
 
-#Compressing images to .chd while preserving original names
+#Compressing .cue images to .chd while preserving original names
 for file in *.cue; do
     if [ -f "$file" ]; then
         base=${file%.cue}
+
+        echo "Compressing $file to ${base}.chd"
+        chdman createcd -i "$file" -o "${base}.chd"
+
+        if [ -f "${base}.chd" ]; then
+            echo "Compression complete. ${base}.chd created."
+        else
+            echo "File compression error $file. Please make sure the image is not damaged."
+        fi
+    fi
+done
+
+#Compressing .iso images to .chd while preserving original names
+for file in *.iso; do
+    if [ -f "$file" ]; then
+        base=${file%.iso}
 
         echo "Compressing $file to ${base}.chd"
         chdman createcd -i "$file" -o "${base}.chd"
